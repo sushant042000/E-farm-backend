ba40b32add5c394eee5ee0f233e83a559a8ad2f1
Sushant Bailkar <sushantbailkar2504@gmail.com>
2024-06-18 23:20:13 +0530

    feat :  user router setup.

--- src/app.js
+++ src/app.js
@@ -21,4 +21,9 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//route declaration
+app.use("/api/v1/user",userRouter);
 export default app;
