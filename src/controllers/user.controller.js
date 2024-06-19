234d12d106dd00be60f10c745ca881095e0115c9
Sushant Bailkar <sushantbailkar2504@gmail.com>
2024-06-19 12:26:01 +0530

    feat : login user api.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens=async(userId)=>{
+try {
+  const user = await User.findById(userId);
+  const access_token = await user.generateAccessToken();
+  const refresh_token = await user.generateRefreshToken();
+
+  user.refreshToken = refresh_token;
+  await user.save({ validateBeforeSave: false });
+  return { access_token, refresh_token };
+} catch (error) {
+  throw new ApiError(
+    500,
+    "Something went wrong while generating access and refresh tokens"
+  );
+}
+}
 const registerUser = asyncHandler(async (req, res) => {
   const { name, surname, password, email, role, phone_number } = req.body;
 
@@ -55,5 +71,43 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "User registered successfully", createdUser));
 });
 
+const loginUser=asyncHandler(async (req, res)=>{
+  const {userName,password}=req.body;
 
-export { registerUser };
+  if(!userName || !password){
+    throw new ApiError(400,"Please provide username and password");
+  }
+  
+  const user = await User.findOne({
+    $or: [{ email: userName }, { phone_number: userName }],
+  });
+
+  if (!user) {
+    throw new ApiError( 400, "User does not exitst");
+  }
+
+  const isPasswordCorrect=await user.isPasswordCorrect(password);
+  if(!isPasswordCorrect){
+    throw new ApiError( 401, "Invalid user credentials");
+  }
+
+  const { access_token, refresh_token }=await generateAccessAndRefreshTokens(user._id);
+  const loggedInUser=await User.findById(user._id).select("-password -refreshToken");
+  const options={
+    httpOnly:true,
+    secure:true
+  }
+  return res.status(200)
+  .cookie("accessToken",access_token,options)
+  .cookie("refreshToken",refresh_token,options)
+  .json(
+    new ApiResponse(200,"success",{
+      loggedInUser,
+      access_token,
+      refresh_token
+    })
+  )
+})
+
+
+export { registerUser , loginUser };
