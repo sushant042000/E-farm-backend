cd5606b8dc218033568bb0f69b36586508a44bf9
Sushant Bailkar <sushantbailkar2504@gmail.com>
2024-06-20 11:17:27 +0530

    feat : logout API

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -107,7 +107,39 @@ const loginUser=asyncHandler(async (req, res)=>{
       refresh_token
     })
   )
+});
+
+const logoutUser=asyncHandler(async(req,res)=>{
+  
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  if (!user) {
+    throw new ApiError(401, "Invalid access token");
+  }
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  
+  user.refreshToken = "";
+  await user.save();
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, "Logged out successfully", null));
 })
 
 
-export { registerUser , loginUser };
+export { registerUser , loginUser , logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { veriefyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(upload.single("avatar"), registerUser);
 
 router.route("/login").post(loginUser);
+
+router.route("/logout").get(veriefyJWT,logoutUser);
 
 
 export default router;
